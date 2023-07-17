21fb71bd7f4ab379913e3ddbd5de180e73a7e3ed
santiarroyave <santi98cv@hotmail.com>
2023-07-17 23:06:12 +0200

    Creacion del componente register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  // ATRIBUTOS
+  email= "";
+  password="";
+
+  // CONSTRUCTOR
+  constructor(private userService:UserService, private router:Router){
+
+  }
+
+  registrar(){
+    alert("Registrando");
+
+    let registro = [
+      {
+        email:this.email, 
+        password:this.password
+      }
+    ];
+
+    this.userService.register(registro).then(response => {
+      console.log(response);
+      this.router.navigate(["/login"]);
+    })
+    .catch(error => console.log(error));
+  };
 }
