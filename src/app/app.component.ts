cf6201ba8d295814e875fa029839857b4b5bb219
santiarroyave <santi98cv@hotmail.com>
2023-07-17 16:23:23 +0200

    Creacion proyecto ng

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sao-fe-gc-ejercicios-T19-rick-and-morty-firebase-07-2023';
+}
